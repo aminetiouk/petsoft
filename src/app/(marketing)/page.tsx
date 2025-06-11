695f22706cb35be9a04c83c5f0bde0091d0e3f9e
aminetiouk <tiouk.amine@gmail.com>
2025-06-11 20:16:26 +0100

    feat(marketing): update landing page buttons to link to signup and login

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -20,9 +21,13 @@ export default function Home() {
           Use PetSoft to easily keep track of pets under your care. Get lifetime
           access for $199.
         </p>
-        <div>
-          <Button variant="outline">Button</Button>
-          <Button variant="outline">Button</Button>
+        <div className="mt-10 space-x-4">
+          <Button asChild>
+            <Link href="/signup">Get started</Link>
+          </Button>
+          <Button asChild variant="secondary">
+            <Link href="/login">Log in</Link>
+          </Button>
         </div>
       </div>
     </main>
