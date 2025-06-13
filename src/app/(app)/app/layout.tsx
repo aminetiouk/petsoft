478349bd91ddcec0b37bba3158d02e5592b4d7c8
aminetiouk <tiouk.amine@gmail.com>
2025-06-13 23:11:00 +0100

    style(layout): ensure app layout fills screen height and adds padding

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: string }) {
     <main>
       <BackgroundHeader />
 
-      <div className="max-w-[1050px] mx-auto px-10">
+      <div className="flex flex-col min-h-screen max-w-[1050px] mx-auto px-10">
         <AppHeader />
         {children}
         <AppFooter />
