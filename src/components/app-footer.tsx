ccc93a5c66ec758e911f0d645d2edcd6766a2ec9
aminetiouk <tiouk.amine@gmail.com>
2025-06-13 23:11:00 +0100

    style(footer): style AppFooter with copyright and border

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,5 +1,7 @@
 export default function AppFooter() {
   return (
-    <div>AppFooter</div>
+    <footer className="mt-auto py-4 text-black/50 border-t border-falu-red/20">
+      &copy; Amine, All right reserved
+    </footer>
   )
 }
