5932857b4728abeb5795d7b690681e1d00cbd484
aminetiouk <tiouk.amine@gmail.com>
2025-06-14 11:39:38 +0100

    feat(components): add ContentBlock, PetDetails, PetList, and SearchForm components

---
+++ src/components/content-block.tsx
@@ -0,0 +1,5 @@
+export default function ContentBlock({children}: {children: React.ReactNode}) {
+  return (
+    <div className="bg-[#f8f9fa] shadow-sm rounded-md overflow-hidden w-full h-full">{children}</div>
+  )
+}
---
+++ src/components/pet-list.tsx
@@ -0,0 +1,9 @@
+export default function PetList() {
+  return (
+    <ul className="bg-white border-b border-black[.08]">
+      <li>
+        <button>benjamin</button>
+      </li>
+    </ul>
+  )
+}
