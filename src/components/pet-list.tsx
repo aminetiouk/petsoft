089ed4dd96f0966ecfb21945cfc8ea40724e59a7
aminetiouk <tiouk.amine@gmail.com>
2025-06-14 19:07:45 +0100

    feat(pet-list): enhance PetList with styled button and image for pet entries

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
+
 export default function PetList() {
   return (
     <ul className="bg-white border-b border-black[.08]">
       <li>
-        <button>benjamin</button>
+        <button className="flex items-center h-[70px] px-5 text-base w-full gap-3 hover:bg-amber-50 focus:bg-amber-50 transition">
+          <Image src='https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png' width={45} height={45} alt="pet placeholder" className="rounded-full object-cover" />
+          <p className="font-semibold">benjamin</p>
+        </button>
       </li>
     </ul>
   )
