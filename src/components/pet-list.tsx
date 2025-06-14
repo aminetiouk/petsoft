8b6729bd0319e3a7ca21fab9c6855e0288df2202
aminetiouk <tiouk.amine@gmail.com>
2025-06-14 19:29:06 +0100

    feat(pet-list): dynamically render pet list based on fetched data

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,14 +1,27 @@
-import Image from "next/image";
+import { pets } from '@/lib/types';
+import Image from 'next/image';
 
-export default function PetList() {
+type PetListProps = {
+  pets: pets[];
+};
+
+export default function PetList({ pets }: PetListProps) {
   return (
     <ul className="bg-white border-b border-black[.08]">
-      <li>
-        <button className="flex items-center h-[70px] px-5 text-base w-full gap-3 hover:bg-amber-50 focus:bg-amber-50 transition">
-          <Image src='https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png' width={45} height={45} alt="pet placeholder" className="rounded-full object-cover" />
-          <p className="font-semibold">benjamin</p>
-        </button>
-      </li>
+      {pets.map(pet => (
+        <li key={pet.id}>
+          <button className="flex items-center h-[70px] px-5 text-base w-full gap-3 hover:bg-amber-50 focus:bg-amber-50 transition">
+            <Image
+              src={pet.imageUrl}
+              width={45}
+              height={45}
+              alt="pet placeholder"
+              className="w-[45px] h-[45px] rounded-full object-cover"
+            />
+            <p className="font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
-  )
+  );
 }
