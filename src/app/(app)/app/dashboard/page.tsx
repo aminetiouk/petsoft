badae161a824dded792e37b6a1fe0930a45a7b39
aminetiouk <tiouk.amine@gmail.com>
2025-06-14 11:40:00 +0100

    feat(dashboard): integrate pet management components (search, list, details, content block) into dashboard layout

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,6 +1,9 @@
-import Branding from "@/components/branding";
-import Stats from "@/components/stats";
-
+import Branding from '@/components/branding';
+import ContentBlock from '@/components/content-block';
+import PetDetails from '@/components/pet-details';
+import PetList from '@/components/pet-list';
+import SearchForm from '@/components/search-form';
+import Stats from '@/components/stats';
 
 export default function Dashboard() {
   return (
@@ -9,6 +12,21 @@ export default function Dashboard() {
         <Branding />
 
         <Stats />
+      </div>
+      <div className="grid grid-cols-3 grid-rows-[45px_1fr] gap-4 h-[600px]">
+        <div className="row-span-1 row-start-1 col-span-1 col-start-1">
+          <SearchForm />
+        </div>
+        <div className='row-start-2 row-span-full col-span-1 col-start-1'>
+          <ContentBlock>
+            <PetList />
+          </ContentBlock>
+        </div>
+        <div className='col-span-full col-start-2 row-start-1 row-span-full'>
+          <ContentBlock>
+            <PetDetails />
+          </ContentBlock>
+        </div>
       </div>
     </main>
   );
