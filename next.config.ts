b4ba7f6673630d9c75497244ef2709ecd584cbea
aminetiouk <tiouk.amine@gmail.com>
2025-06-14 19:29:06 +0100

    config: allow images from images.unsplash.com in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "bytegrad.com"
+        protocol: 'https',
+        hostname: 'bytegrad.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
       }
     ]
   }
