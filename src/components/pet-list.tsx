5959165278a05087467c4b503ec6cf2101136397
aminetiouk <tiouk.amine@gmail.com>
2025-06-15 18:38:15 +0100

    feat(pet-list): enable pet selection by calling handleChangeSelectedPetId on button click

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -4,12 +4,12 @@ import { usePetContext } from '@/lib/hooks';
 import Image from 'next/image';
 
 export default function PetList() {
-  const { pets } = usePetContext();
+  const { pets, handleChangeSelectedPetId } = usePetContext();
   return (
     <ul className="bg-white">
       {pets.map(pet => (
         <li key={pet.id}>
-          <button className="flex items-center h-[70px] px-5 text-base w-full gap-3 border-b border-black[.08] hover:bg-amber-50 focus:bg-amber-50 transition">
+          <button onClick={() => handleChangeSelectedPetId(pet.id)} className="flex items-center h-[70px] px-5 text-base w-full gap-3 border-b border-black[.08] hover:bg-amber-50 focus:bg-amber-50 transition">
             <Image
               src={pet.imageUrl}
               width={45}
