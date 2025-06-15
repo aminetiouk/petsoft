694d05df6ac7f93f62ad994af5b7145791664466
aminetiouk <tiouk.amine@gmail.com>
2025-06-15 11:11:58 +0100

    feat(layout): fetch pet data at the layout level and provide it via PetContextProvider

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -1,15 +1,22 @@
 import AppFooter from '@/components/app-footer';
 import AppHeader from '@/components/app-header';
 import BackgroundHeader from '@/components/background-header';
+import PetContextProvider from '@/components/contexts/pet-context-provider';
+import { TPets } from '@/lib/types';
 
-export default function Layout({ children }: { children: string }) {
+export default async function Layout({ children }: { children: string }) {
+  const response = await fetch('https://bytegrad.com/course-assets/projects/petsoft/api/pets');
+    if(!response.ok) {
+      throw new Error("Could not fetch pets");
+    }
+    const data: TPets[] = await response.json();
   return (
     <main>
       <BackgroundHeader />
 
       <div className="flex flex-col min-h-screen max-w-[1050px] mx-auto px-4">
         <AppHeader />
-        {children}
+        <PetContextProvider data={data}>{children}</PetContextProvider>
         <AppFooter />
       </div>
     </main>
