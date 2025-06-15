7f0ee0774e1aea2cfdee6722fda62d7c92fd0a40
aminetiouk <tiouk.amine@gmail.com>
2025-06-15 20:05:07 +0100

    feat(pet-list): highlight the currently selected pet in the list

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,15 +1,24 @@
-"use client"
+'use client';
 
 import { usePetContext } from '@/lib/hooks';
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
 
 export default function PetList() {
-  const { pets, handleChangeSelectedPetId } = usePetContext();
+  const { pets, selectedPetId, handleChangeSelectedPetId } = usePetContext();
   return (
     <ul className="bg-white">
       {pets.map(pet => (
         <li key={pet.id}>
-          <button onClick={() => handleChangeSelectedPetId(pet.id)} className="flex items-center h-[70px] px-5 text-base w-full gap-3 border-b border-black[.08] hover:bg-amber-50 focus:bg-amber-50 transition">
+          <button
+            onClick={() => handleChangeSelectedPetId(pet.id)}
+            className={cn(
+              'flex items-center h-[70px] px-5 text-base w-full gap-3 border-b border-black[.08] hover:bg-amber-50 focus:bg-amber-50 transition',
+              {
+                'bg-amber-50': selectedPetId === pet.id
+              }
+            )}
+          >
             <Image
               src={pet.imageUrl}
               width={45}
