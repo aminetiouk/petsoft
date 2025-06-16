3473d4d34cdafaa7dc563f57fdccd2737bdb014c
aminetiouk <tiouk.amine@gmail.com>
2025-06-16 15:32:09 +0100

    feat(search-form): integrate with useSearchContext for managing search input

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,7 +1,18 @@
+"use client"
+
+import { useSearchContext } from "@/lib/hooks";
+
 export default function SearchForm() {
+  const {searchQuery, handleChangeSearchQuery} = useSearchContext();
   return (
     <form className="w-full h-full">
-      <input className="w-full h-full bg-white/50 rounded-md" />
+      <input
+        className="w-full h-full bg-white/50 hover:bg-white/60 focus:bg-white/70 rounded-md px-5 outline-none transition placeholder:text-black/30"
+        placeholder="Search pets"
+        type="search"
+        value={searchQuery}
+        onChange={(e) => handleChangeSearchQuery(e.target.value)}
+      />
     </form>
-  )
+  );
 }
