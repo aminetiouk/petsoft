be197a393f4cd31ccc07cfc65099c37322e1cc54
aminetiouk <tiouk.amine@gmail.com>
2025-06-16 16:45:49 +0100

    feat(account): update account page with H1 and ContentBlock for login display

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,5 +1,13 @@
+import ContentBlock from '@/components/content-block';
+import H1 from '@/components/h1';
+
 export default function Account() {
   return (
-    <div>Account Page</div>
-  )
+    <main>
+      <H1 className="my-8">Your account</H1>
+      <ContentBlock className="h-[500px] flex items-center justify-center text-3xl text-black/50">
+        <p>Login...</p>
+      </ContentBlock>
+    </main>
+  );
 }
