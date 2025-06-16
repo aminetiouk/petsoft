5493c02cbf54f02033479518e6f8153f066cfdce
aminetiouk <tiouk.amine@gmail.com>
2025-06-16 15:32:09 +0100

    refactor(hooks): update usePetContext and introduce useSearchContext for new context names

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,11 +1,22 @@
-import { PetContext } from "@/components/contexts/pet-context-provider";
+import { petContext } from "@/components/contexts/pet-context-provider";
+import { searchContext } from "@/components/contexts/search-context-provider";
 import { useContext } from "react";
 
 export function usePetContext() {
-  const context = useContext(PetContext);
+  const context = useContext(petContext);
   
   if (!context) {
     throw new Error('usePetContext must be used within a PetContextProvider');
+  }
+
+  return context;
+}
+
+export function useSearchContext() {
+  const context = useContext(searchContext)
+
+  if (!context) {
+    throw new Error("useSearchContext must be used within SearchContextProvider")
   }
 
   return context;
