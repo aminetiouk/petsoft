98ea5365b1f4772184137c518d2c23704930e647
aminetiouk <tiouk.amine@gmail.com>
2025-06-16 15:32:09 +0100

    feat(layout): wrap children with SearchContextProvider

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -2,21 +2,26 @@ import AppFooter from '@/components/app-footer';
 import AppHeader from '@/components/app-header';
 import BackgroundHeader from '@/components/background-header';
 import PetContextProvider from '@/components/contexts/pet-context-provider';
+import SearchContextProvider from '@/components/contexts/search-context-provider';
 import { TPets } from '@/lib/types';
 
 export default async function Layout({ children }: { children: string }) {
-  const response = await fetch('https://bytegrad.com/course-assets/projects/petsoft/api/pets');
-    if(!response.ok) {
-      throw new Error("Could not fetch pets");
-    }
-    const data: TPets[] = await response.json();
+  const response = await fetch(
+    'https://bytegrad.com/course-assets/projects/petsoft/api/pets'
+  );
+  if (!response.ok) {
+    throw new Error('Could not fetch pets');
+  }
+  const data: TPets[] = await response.json();
   return (
     <main>
       <BackgroundHeader />
 
       <div className="flex flex-col min-h-screen max-w-[1050px] mx-auto px-4">
         <AppHeader />
-        <PetContextProvider data={data}>{children}</PetContextProvider>
+        <SearchContextProvider>
+          <PetContextProvider data={data}>{children}</PetContextProvider>
+        </SearchContextProvider>
         <AppFooter />
       </div>
     </main>
