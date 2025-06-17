9a3568b2f47c9f6127a8e2810f48c182bc8496bd
aminetiouk <tiouk.amine@gmail.com>
2025-06-17 13:11:32 +0100

    feat(pet-button): integrate dialogs with PetButton for add/edit pet actions

--- src/components/petButton.tsx
+++ src/components/petButton.tsx
@@ -1,5 +1,13 @@
 import { Button } from './ui/button';
 import Image from 'next/image';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger
+} from './ui/dialog';
+import { DialogTitle } from '@radix-ui/react-dialog';
+import PetForm from './petForm';
 
 type TPetButton = {
   actionType: 'icon' | 'edit' | 'checkout';
@@ -12,22 +20,36 @@ export default function PetButton({
   children,
   onClick
 }: TPetButton) {
-  if (actionType === 'icon') {
+  if (actionType === 'icon' || actionType === 'edit') {
     return (
-      <Button size="icon">
-        <Image
-          src="/plus.svg"
-          width={15}
-          height={15}
-          alt="plus icon"
-          className="w-6 h-6"
-        />
-      </Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          {actionType === 'icon' ? (
+            <Button size="icon" className="cursor-pointer">
+              <Image
+                src="/plus.svg"
+                width={15}
+                height={15}
+                alt="plus icon"
+                className="w-6 h-6"
+              />
+            </Button>
+          ) : (
+            <Button variant="secondary">{children}</Button>
+          )}
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              {actionType === 'icon' ? 'Add new pet' : 'Edit the pet'}
+            </DialogTitle>
+          </DialogHeader>
+          <PetForm />
+        </DialogContent>
+      </Dialog>
     );
   }
-  if (actionType === 'edit') {
-    return <Button variant="secondary">{children}</Button>;
-  }
+
   if (actionType === 'checkout') {
     return (
       <Button variant="secondary" onClick={onClick}>
