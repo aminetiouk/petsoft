400b7e071206ef837787c2db3f0c175f0ba8c0e7
aminetiouk <tiouk.amine@gmail.com>
2025-06-18 12:22:29 +0100

    feat(pet-form): implement PetForm for adding and editing pet details

--- src/components/petForm.tsx
+++ src/components/petForm.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+import { Textarea } from './ui/textarea';
 
-export default function PetForm() {
+type TPetFormProps = {
+  actionType: 'add' | 'edit';
+};
+
+export default function PetForm({ actionType }: TPetFormProps) {
   return (
-    <div>form ....</div>
-  )
+    <form className="flex flex-col">
+      <div className="space-y-3">
+        <div className="space-y-2">
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" type="text" />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="ownerName">Owner Name</Label>
+          <Input id="ownerName" type="text" />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="imageUrl">Image Url</Label>
+          <Input id="imageUrl" type="text" />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="age">Age</Label>
+          <Input id="age" type="number" />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="notes">Notes</Label>
+          <Textarea id="notes" rows={3} />
+        </div>
+      </div>
+      <Button type='submit' className="mt-5 self-end">
+        {actionType === 'add' ? 'Add New Pet' : 'Edit Pet'}
+      </Button>
+    </form>
+  );
 }
