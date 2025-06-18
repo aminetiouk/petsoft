f45dbc5f27d23cb5e7d586da52abbfd7ae89b128
aminetiouk <tiouk.amine@gmail.com>
2025-06-18 16:45:21 +0100

    style(button): slightly reduce default icon size for consistency

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -25,7 +25,7 @@ const buttonVariants = cva(
         default: 'h-10 px-6 py-4 has-[>svg]:px-3',
         sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        icon: 'size-15'
+        icon: 'size-13'
       }
     },
     defaultVariants: {
