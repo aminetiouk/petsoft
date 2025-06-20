814d3a69515d2986a1ac66c30dd7414829755e8f
aminetiouk <tiouk.amine@gmail.com>
2025-06-20 13:08:53 +0100

    refactor(data): fetch pets directly from database using Prisma in app layout

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -3,16 +3,11 @@ import AppHeader from '@/components/app-header';
 import BackgroundHeader from '@/components/background-header';
 import PetContextProvider from '@/components/contexts/pet-context-provider';
 import SearchContextProvider from '@/components/contexts/search-context-provider';
-import { TPets } from '@/lib/types';
+import prisma from '@/lib/prisma';
 
 export default async function Layout({ children }: { children: string }) {
-  const response = await fetch(
-    'https://bytegrad.com/course-assets/projects/petsoft/api/pets'
-  );
-  if (!response.ok) {
-    throw new Error('Could not fetch pets');
-  }
-  const data: TPets[] = await response.json();
+  
+  const pets = await prisma.pet.findMany();
   return (
     <main>
       <BackgroundHeader />
@@ -20,7 +15,7 @@ export default async function Layout({ children }: { children: string }) {
       <div className="flex flex-col min-h-screen max-w-[1050px] mx-auto px-4">
         <AppHeader />
         <SearchContextProvider>
-          <PetContextProvider data={data}>{children}</PetContextProvider>
+          <PetContextProvider data={pets}>{children}</PetContextProvider>
         </SearchContextProvider>
         <AppFooter />
       </div>
