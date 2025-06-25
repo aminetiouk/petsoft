2448c0797f173c5f05d3b5093a203eb9f28be38c
aminetiouk <tiouk.amine@gmail.com>
2025-06-25 14:49:24 +0100

    refactor(pet-form-btn): add type definition for actionType prop

--- src/components/pet-form-btn.tsx
+++ src/components/pet-form-btn.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Button } from './ui/button';
 import { useFormStatus } from 'react-dom';
 
-export default function PetFormBtn({actionType}) {
+type TPetFormBtnProps = {
+  actionType: 'add' | 'edit';
+}
+
+export default function PetFormBtn({actionType}: TPetFormBtnProps) {
  const {pending} =  useFormStatus();
   return (
     <Button type="submit" disabled={pending} className="mt-5 self-end">
