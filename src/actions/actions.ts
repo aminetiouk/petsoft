357d6f9a0dfd35b5cebe3d34860d9480e7d424e5
aminetiouk <tiouk.amine@gmail.com>
2025-06-25 12:01:18 +0100

    refactor(actions): add simulated delay and error handling to addPet server action

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,18 +1,27 @@
 "use server"
 
 import prisma from "@/lib/prisma"
+import { sleep } from "@/lib/utils";
 import { revalidatePath } from "next/cache";
 
 export async function addPet(formData) {
-  await prisma.pet.create({
-    data: {
-      name: formData.get("name"),
-      ownerName: formData.get("ownerName"),
-      age: parseInt(formData.get("age")),
-      imageUrl: formData.get("imageUrl") || '/placeholder.svg',
-      notes: formData.get("notes"),
-    },
-  });
+  await sleep(2000)
+  try {
 
-  revalidatePath('app', 'layout');
+    await prisma.pet.create({
+      data: {
+        name: formData.get("name"),
+        ownerName: formData.get("ownerName"),
+        // age: parseInt(formData.get("age")),
+        imageUrl: formData.get("imageUrl") || '/placeholder.svg',
+        notes: formData.get("notes"),
+      },
+    });
+    
+  }catch (error) {
+    return {
+      message: 'could not add pet'
+    }
+  }
+    revalidatePath('app', 'layout');
 }
