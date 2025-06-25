1bd8c2a56a15ff80f275bfdefac678739d2feb0f
aminetiouk <tiouk.amine@gmail.com>
2025-06-25 12:22:04 +0100

    feat(actions): implement editPet server action with Prisma update and error handling

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,27 +1,48 @@
-"use server"
+'use server';
 
-import prisma from "@/lib/prisma"
-import { sleep } from "@/lib/utils";
-import { revalidatePath } from "next/cache";
+import prisma from '@/lib/prisma';
+import { sleep } from '@/lib/utils';
+import { revalidatePath } from 'next/cache';
 
 export async function addPet(formData) {
-  await sleep(2000)
+  await sleep(2000);
   try {
-
     await prisma.pet.create({
       data: {
-        name: formData.get("name"),
-        ownerName: formData.get("ownerName"),
-        // age: parseInt(formData.get("age")),
-        imageUrl: formData.get("imageUrl") || '/placeholder.svg',
-        notes: formData.get("notes"),
-      },
+        name: formData.get('name'),
+        ownerName: formData.get('ownerName'),
+        age: parseInt(formData.get('age')),
+        imageUrl: formData.get('imageUrl') || '/placeholder.svg',
+        notes: formData.get('notes')
+      }
     });
-    
-  }catch (error) {
+  } catch (error) {
     return {
       message: 'could not add pet'
-    }
+    };
   }
-    revalidatePath('app', 'layout');
+  revalidatePath('app', 'layout');
+}
+
+export async function editPet(petId, formData) {
+  try {
+    await prisma.pet.update({
+      where: {
+        id: petId
+      },
+      data: {
+        name: formData.get('name'),
+        ownerName: formData.get('ownerName'),
+        age: parseInt(formData.get('age')),
+        imageUrl: formData.get('imageUrl') || '/placeholder.svg',
+        notes: formData.get('notes')
+      }
+    });
+  } catch (error) {
+    return {
+      message: 'Could not edit pet'
+    };
+  }
+
+  revalidatePath('app', 'layout');
 }
