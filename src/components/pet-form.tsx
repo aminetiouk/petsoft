913725e4cc493f04a3838eef281f8202d77cf23c
aminetiouk <tiouk.amine@gmail.com>
2025-06-27 11:32:23 +0100

    refactor(component): rename PetForm.tsx to pet-form.tsx

---
+++ src/components/pet-form.tsx
@@ -0,0 +1,118 @@
+'use client';
+
+import { usePetContext } from '@/lib/hooks';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+import { Textarea } from './ui/textarea';
+import { addPet, editPet } from '@/actions/actions';
+import PetFormBtn from './pet-form-btn';
+import { toast } from 'sonner';
+
+type TPetFormProps = {
+  actionType: 'add' | 'edit';
+  onFormSubmission: () => void;
+};
+
+export default function PetForm({
+  actionType,
+  onFormSubmission
+}: TPetFormProps) {
+  const { selectedPet } = usePetContext();
+
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+
+  //   const formData = new FormData(event.currentTarget);
+  //   const pet = {
+  //     name: formData.get('name') as string,
+  //     ownerName: formData.get('ownerName') as string,
+  //     imageUrl: (formData.get('imageUrl') as string) || '/placeholder.svg',
+  //     age: Number(formData.get('age')),
+  //     notes: formData.get('notes') as string
+  //   };
+
+  //   if (actionType === 'add') {
+  //     handleAddNewPet(pet);
+  //   } else if (actionType === 'edit') {
+  //     handleEditPet(selectedPet!.id, pet);
+  //   }
+
+  //   onFormSubmission();
+  // };
+  return (
+    <form
+      action={async formData => {
+        if (actionType === 'add') {
+          const error = await addPet(formData);
+          if (error) {
+            toast.warning(error.message);
+            return;
+          }
+          onFormSubmission();
+        } else {
+          const error = await editPet(selectedPet?.id, formData);
+          if (error) {
+            toast.warning(error.message);
+            return;
+          }
+        }
+        onFormSubmission();
+      }}
+      className="flex flex-col"
+    >
+      <div className="space-y-3">
+        <div className="space-y-2">
+          <Label htmlFor="name">Name</Label>
+          <Input
+            id="name"
+            name="name"
+            type="text"
+            required
+            defaultValue={actionType === 'edit' ? selectedPet?.name : ''}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="ownerName">Owner Name</Label>
+          <Input
+            id="ownerName"
+            name="ownerName"
+            type="text"
+            required
+            defaultValue={actionType === 'edit' ? selectedPet?.ownerName : ''}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="imageUrl">Image Url</Label>
+          <Input
+            id="imageUrl"
+            name="imageUrl"
+            type="text"
+            defaultValue={actionType === 'edit' ? selectedPet?.imageUrl : ''}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="age">Age</Label>
+          <Input
+            id="age"
+            name="age"
+            type="number"
+            required
+            defaultValue={actionType === 'edit' ? selectedPet?.age : ''}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="notes">Notes</Label>
+          <Textarea
+            id="notes"
+            name="notes"
+            rows={3}
+            required
+            defaultValue={actionType === 'edit' ? selectedPet?.notes : ''}
+          />
+        </div>
+      </div>
+
+      <PetFormBtn actionType={actionType} />
+    </form>
+  );
+}
