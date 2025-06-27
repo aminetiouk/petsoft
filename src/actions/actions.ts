6a12ffa38b73cb254b001d30ec9fed74e051bd84
aminetiouk <tiouk.amine@gmail.com>
2025-06-27 22:42:12 +0100

    refactor(actions): modify addPet and editPet to accept structured pet data

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -4,17 +4,11 @@ import prisma from '@/lib/prisma';
 import { sleep } from '@/lib/utils';
 import { revalidatePath } from 'next/cache';
 
-export async function addPet(formData) {
+export async function addPet(pet) {
   await sleep(2000);
   try {
     await prisma.pet.create({
-      data: {
-        name: formData.get('name'),
-        ownerName: formData.get('ownerName'),
-        age: parseInt(formData.get('age')),
-        imageUrl: formData.get('imageUrl') || '/placeholder.svg',
-        notes: formData.get('notes')
-      }
+      data: pet,
     });
   } catch (error) {
     return {
@@ -24,20 +18,14 @@ export async function addPet(formData) {
   revalidatePath('app', 'layout');
 }
 
-export async function editPet(petId, formData) {
+export async function editPet(petId, newPetData) {
   await sleep(2000);
   try {
     await prisma.pet.update({
       where: {
         id: petId
       },
-      data: {
-        name: formData.get('name'),
-        ownerName: formData.get('ownerName'),
-        age: parseInt(formData.get('age')),
-        imageUrl: formData.get('imageUrl') || '/placeholder.svg',
-        notes: formData.get('notes')
-      }
+      data: newPetData,
     });
   } catch (error) {
     return {
