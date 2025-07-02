34e4429faeaa6816c5885d93f2e92bcd5c691b39
aminetiouk <tiouk.amine@gmail.com>
2025-07-02 21:30:31 +0100

    refactor(pet-form-btn): remove unused useFormStatus hook

--- src/components/pet-form-btn.tsx
+++ src/components/pet-form-btn.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Button } from './ui/button';
-import { useFormStatus } from 'react-dom';
 
 type TPetFormBtnProps = {
   actionType: 'add' | 'edit';
 }
 
 export default function PetFormBtn({actionType}: TPetFormBtnProps) {
- const {pending} =  useFormStatus();
   return (
-    <Button type="submit" disabled={pending} className="mt-5 self-end">
+    <Button type="submit" className="mt-5 self-end">
       {actionType === 'add' ? 'Add New Pet' : 'Edit Pet'}
     </Button>
   );
