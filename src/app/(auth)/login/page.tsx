f61bbacc955b7de25dc914a8a4f3907b10829611
aminetiouk <tiouk.amine@gmail.com>
2025-07-07 22:19:45 +0100

    refactor(auth): integrate AutForm component into login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import AutForm from '@/components/aut-form'
+import H1 from '@/components/h1'
+import Link from 'next/link'
 
 export default function Login() {
   return (
-    <div>Login page</div>
+    <main className='space-y-2'>
+      <H1 className='mb-4'>Log In</H1>
+
+      <AutForm />
+
+      <p>
+        No account yet?{" "}
+        <Link href='/signup' className='mt-6 text-sm text-zinc-500'>
+        Sign up
+        </Link>
+      </p>
+    </main>
   )
 }
