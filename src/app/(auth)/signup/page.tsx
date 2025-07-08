a1f71989e571977aa0e43b8cd3cac433f1842aaa
aminetiouk <tiouk.amine@gmail.com>
2025-07-08 20:47:11 +0100

    feat(signup): integrate AutForm component and pass 'Sign up' as auth prop

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,20 @@
-export default function Signup() {
+import AutForm from '@/components/aut-form';
+import H1 from '@/components/h1';
+import Link from 'next/link';
+
+export default function Login() {
   return (
-    <div>Signup page</div>
-  )
+    <main className="space-y-2">
+      <H1 className="mb-4">Sign up</H1>
+
+      <AutForm auth='Sign up' />
+
+      <p>
+        Already have an account?{' '}
+        <Link href="/login" className="mt-6 text-sm text-zinc-500">
+          Log in
+        </Link>
+      </p>
+    </main>
+  );
 }
