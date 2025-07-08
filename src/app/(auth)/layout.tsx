93c1e0ba8766bd34db3f7117258dcd7e2d4f631e
aminetiouk <tiouk.amine@gmail.com>
2025-07-08 20:47:11 +0100

    style(auth): explicitly set logo width in authentication layout for visual consistency

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import Logo from '@/components/logo';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex flex-col gap-y-5 justify-center items-center min-h-screen'>
-      <Logo />
+      <Logo width={50} />
       {children}
     </div>
   );
