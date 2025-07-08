7f176ee5fc79e3b6709ac992f3a4b8b7c65955f0
aminetiouk <tiouk.amine@gmail.com>
2025-07-08 20:47:11 +0100

    feat(login): integrate AutForm component and pass 'Log In' as auth prop

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -7,7 +7,7 @@ export default function Login() {
     <main className='space-y-2'>
       <H1 className='mb-4'>Log In</H1>
 
-      <AutForm />
+      <AutForm auth='Log In' />
 
       <p>
         No account yet?{" "}
