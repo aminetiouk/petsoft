7686e8e1331accdcff0cea0903b0f73edc0438e6
aminetiouk <tiouk.amine@gmail.com>
2025-07-10 21:51:27 +0100

    feat(auth): configure NextAuth.js with Credentials provider and authorization callback

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,46 @@
 import NextAuth from 'next-auth';
 import NextAuthConfig from 'next-auth';
-
-export const { auth } = NextAuth(config);
+import Credentials from 'next-auth/providers/credentials';
+import prisma from './prisma';
+import bcrypt from 'bcrypt';
 
 const config = {
   pages: {
     signIn: '/login'
   },
-  providers: [],
-  callbacks: 
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const { email, password } = credentials;
+
+        const user = prisma.user.findUnique({
+          where: {
+            email
+          }
+        });
+        if (!user) {
+          console.log('No user found');
+          return null;
+        }
+
+        const passwordsMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword
+        );
+      }
+    }) 
+  ],
+  callbacks: {
+    authorized: ({ request }) => {
+      const isTryingToAccessApp = request.nextUrl.pathname.includes('/app');
+
+      if (isTryingToAccessApp) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
 } satisfies NextAuthConfig;
+
+export const { auth } = NextAuth(config);
